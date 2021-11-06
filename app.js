557d19d0af9f5c5982090f4394419e8fecc5fd6f
BrenoLins100 <brenolins100@gmail.com>
2021-11-05 21:22:37 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,54 @@
+/* Example in Node.js ES6 using request-promise */
+const express = require('express');
+const app = express();
+
+const rp = require('request-promise');
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  qs: {
+    'start': '1',
+    'limit': '5000',
+    'convert': 'USD'
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': '76aa0192-a4a6-4802-8ef6-a5403a98252a'
+  },
+  json: true,
+  gzip: true
+};
+
+const PORT = process.env.PORT || 8877;
+
+app.get('/',(req,res)=>{
+  res.json({
+    msg: 'ok'
+  })
+})
+
+app.get('/shib',(req,res)=>{
+
+  rp(requestOptions).then(response => {
+  
+    const dados = response.data
+  
+    dados.map((x)=>{
+      if(x.symbol == 'SHIB'){
+        console.log(x.name)
+        res.json(
+          x
+        )
+      }
+    })
+
+  }).catch((err) => {
+    console.log('API call error:', err.message);
+  });
+
+  
+})
+
+app.listen(PORT, ()=>{
+    //console.log('Porta'+PORT)
+});
+
