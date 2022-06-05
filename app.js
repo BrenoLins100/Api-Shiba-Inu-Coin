1054f806d1b3ddbd5db51ef3a2c1a24861daad69
Breno Lins <brenolins100@gmail.com>
2022-06-05 18:10:16 -0300

    Alterações na configuração de porta

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const app = express();
 const rp = require('request-promise');
 const requestOptions = {
   method: 'GET',
-  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  uri: 'http://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
   qs: {
     'start': '1',
     'limit': '5000',
     'convert': 'USD'
   },
   headers: {
-    'X-CMC_PRO_API_KEY': toString(process.env.API_KEY),
+    'X-CMC_PRO_API_KEY': process.env.API_KEY,
   },
   json: true,
   gzip: true
